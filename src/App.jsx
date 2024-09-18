429f6a4349096510a12582b7d0548a790c2c7a9c
Danilashionshion <148801954+Danilashionshion@users.noreply.github.com>
2024-09-18 17:09:22 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,65 @@
 import { useState } from 'react'
+
+
+function App() {
+
+
+ 
+  const[lista,setlista] = useState([])
+  const[novatarefa,setnovatarefa] = useState("")
+  const[count,setcount] = useState(0)
+  const[uncheck,setuncheck] = useState(0)
+  
+  function check(){
+    let checkbox = document.querySelectorAll('.checker')
+    checkbox.forEach((el) => {
+      if(event.target.checked){
+        setuncheck(uncheck - 1)
+      }else{
+        setuncheck(uncheck + 1)
+      }
+    })
+  }
+  function adicionar(){
+    setlista([...lista, novatarefa])
+    setcount(count+1)
+    setnovatarefa("")
+  }
+  function excluir(index){
+      const novaLista = lista.filter((_, i) => i !== index);
+      setlista(novaLista);
+      setcount(count - 1);
+    
+  }
+
+  console.log(lista)  
+  return (
+    <>
+          <span>{uncheck}</span>
+          <br/>
+          <span>{count}</span>
+          <input type='text' value={novatarefa} onChange={e => setnovatarefa(e.target.value)} />
+          <button color="secondary" onClick={adicionar} >Adicionar</button>
+          
+          
+          <div>
+
+            {lista.map((lista, index) => (
+              <div key={index}>
+                <input type='checkbox' className='checker' onChange={check} />  <span>{lista}</span> <button variant="outlined" color="error" onClick={() => excluir(index)}>remover</button> <br/>
+              </div>
+            ))}
+          </div>
+       
+    </>
+  )
+}
+
+export default App
+
+
+
+/*import { useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
@@ -46,3 +107,4 @@ function App() {
 }
 
 export default App
+*/
